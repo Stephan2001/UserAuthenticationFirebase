c854cfd1f7c1994bc631b64f7aee6aa11290a49d
Clene <104350902+Clene1@users.noreply.github.com>
2024-04-15 09:59:49 +0200

    buttons added

--- app/src/main/java/com/example/userauthentication/EmailPasswordActivity.kt
+++ app/src/main/java/com/example/userauthentication/EmailPasswordActivity.kt
@@ -1,6 +1,7 @@
 package com.example.userauthentication
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,20 @@ class EmailPasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_email_password)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener ()
+        {
+
+        }
+
+        val btnReg = findViewById<Button>(R.id.btnReg)
+        btnReg.setOnClickListener ()
+        {
+
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
