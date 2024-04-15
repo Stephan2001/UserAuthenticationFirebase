6a84937d6271e5ce73df53020783a512b6b23e9f
ST10219213 <moolmans20013@gmail.com>
2024-04-15 10:51:32 +0200

    login success

--- app/src/main/java/com/example/userauthentication/LoginSuccessful.kt
+++ app/src/main/java/com/example/userauthentication/LoginSuccessful.kt
@@ -1,10 +1,13 @@
 package com.example.userauthentication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class LoginSuccessful : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +22,19 @@ class LoginSuccessful : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        checkCurrentUser()
+    }
+
+    private fun checkCurrentUser() {
+        // [START check_current_user]
+        val user = Firebase.auth.currentUser
+        if (user != null) {
+            val email: TextView = findViewById(R.id.txtEmail)
+            email.text = email.text.toString()
+        } else {
+            // No user is signed in
+        }
+        // [END check_current_user]
     }
 }
